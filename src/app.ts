61e059ee718695d9403a467ef5a07aef27afb6d1
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2023-07-10 11:38:00 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express(); 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.get('/',async (req,res)=>{
+    res.send("Hello World Node js")
+});
+
+app.get('/user',(req,res)=>{
+    res.send("Hello Users")
+});
+
+app.get('*', function(req, res){
+    res.send("404 : Page Not Found ");
+  });
+  
+const port = process.env.PORT || 2000;
+app.listen(port,()=>{
+    console.log("server running on port",port);
+});
+
+module.exports = app;
