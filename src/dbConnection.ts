70fd8d59c8d8e40a62684307dfebc19913bdefc8
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2023-07-14 10:58:34 +0530

    quick fixes

--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -1,11 +1,13 @@
 import {Sequelize} from 'sequelize';
 
+import pg from "pg";
 export const sequelize = new Sequelize(
   'verceldb',
   'default',
   'rOiTy8wQEf5a',
   {
     dialect: 'postgres',
+    dialectModule: pg,
     host: 'ep-dark-sound-972921-pooler.us-east-1.postgres.vercel-storage.com',
     dialectOptions: {
         ssl: {
